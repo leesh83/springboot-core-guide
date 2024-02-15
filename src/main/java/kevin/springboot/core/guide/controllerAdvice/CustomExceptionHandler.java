10175fc03b9cac42781fd20ec7a94cefe2d9c32a
ironmask431 <ironmask431@gmail.com>
2024-02-15 22:49:41 +0900

    feat: lombok validation exception handler

--- src/main/java/kevin/springboot/core/guide/controllerAdvice/CustomExceptionHandler.java
+++ src/main/java/kevin/springboot/core/guide/controllerAdvice/CustomExceptionHandler.java
@@ -1,4 +1,4 @@
-package kevin.springboot.core.guide.configuration;
+package kevin.springboot.core.guide.controllerAdvice;
 
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -21,6 +22,18 @@ public class CustomExceptionHandler {
         HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
 
         ExceptionResponse response = new ExceptionResponse(httpStatus.value(), httpStatus.getReasonPhrase(), e.getMessage());
+
+        return new ResponseEntity<>(response, new HttpHeaders(), httpStatus);
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request){
+        log.error("advice 내 handleMethodArgumentNotValidException 호출 url : {}, exception message : {}", request.getRequestURI(), e.getMessage());
+
+        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+
+        ExceptionResponse response = new ExceptionResponse(httpStatus.value(), httpStatus.getReasonPhrase(),
+                e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
 
         return new ResponseEntity<>(response, new HttpHeaders(), httpStatus);
     }
